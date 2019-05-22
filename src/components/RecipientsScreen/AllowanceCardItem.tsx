1b92d3831b9120ef7b94c84d14eff801a4abfd21
yuki <yuukikokubo.pg@gmail.com>
2019-05-22 13:39:27 +0900

    [update] お小遣いのボタンの見た目を調整しました。

--- src/components/RecipientsScreen/AllowanceCardItem.tsx
+++ src/components/RecipientsScreen/AllowanceCardItem.tsx
@@ -4,6 +4,7 @@ import {
   View,
   Text,
   Icon,
+  Grid,
 } from "native-base";
 import { NavigationScreenProp } from "react-navigation";
 import { StyleSheet } from "react-native";
@@ -20,20 +21,22 @@ export default class AllowanceCard extends React.Component<Props> {
     const { allowance, navigation } = this.props;
     const { title, amount, isDone } = allowance;
     const cardStyle = isDone ? styles.CompletedButton : this._chooseButtonStyle();
+    const textStyle = isDone ? styles.CompletedText : styles.Text;
+
     return(
       <View style={{ paddingRight: 5, paddingLeft: 5}}>
         <Button style={cardStyle}
+          bordered={true}
           onPress={() =>
             navigation.navigate("AllowancePropertyModal", {
               allowanceId: this.props.allowance.id,
               monthId: navigation.getParam("monthId")
           })} 
         >
-          <View style={{ padding: 5}}>
-            <Text style={styles.ButtonText}>{title}</Text>
-            <Text style={styles.ButtonText}>{amount}</Text>
-            {/** TODO tagによるアイコンをつける */}
-            {}
+          <View style={{ padding: 5, }}>
+                {this._chooseTagsIcon()}
+                <Text style={[textStyle, { flex: 1, fontSize: 24 }]}>{title.slice(0, 12)}</Text>
+                <Text style={[textStyle, { flex: 1, paddingLeft: 10 }]}>{amount}円</Text>
           </View>
         </Button>
       </View>
@@ -51,18 +54,36 @@ export default class AllowanceCard extends React.Component<Props> {
     else
       return styles.DefaultButton;
   }
+
+  _chooseTagsIcon() {
+    const { tags, isDone } = this.props.allowance;
+    const iconStyle = [isDone ? styles.CompletedTagIcon : styles.TagIcon, { flex: 1 }];
+    if(tags === "hobby")
+      return <Icon type="FontAwesome" name="smile-o" style={iconStyle} />
+    else if(tags === "food")
+      return <Icon type="MaterialCommunityIcons" name="food-fork-drink" style={iconStyle} />
+    else if(tags === "transport")
+      return <Icon type="MaterialCommunityIcons" name="train" style={iconStyle} />
+    else
+      return <Icon type="MaterialCommunityIcons" name="coin" style={iconStyle} />
+  }
+
 }
 
 const styles = StyleSheet.create({
-  ButtonText: {
-    color: "white",
+  CompletedText: {
+    color: "#e6e6e6",
     fontSize: 20,
     fontWeight: "700",
-    flex: 1
-
+  },
+  Text: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "700"
   },
   CompletedButton: {
-    backgroundColor: "gray",
+    backgroundColor: "#d3d3d3",
+    borderColor: "gray",
     flex: 1,
     height: 140,
     minWidth: 150,
@@ -71,6 +92,7 @@ const styles = StyleSheet.create({
   },
   DefaultButton: {
     backgroundColor: "#483d8b",
+    borderColor: "black",
     flex: 1,
     height: 140,
     minWidth: 150,
@@ -78,15 +100,37 @@ const styles = StyleSheet.create({
   },
   HobbyButton: {
     backgroundColor: "#CAB783",
+    borderColor: "black",
     flex: 1,
     height: 140,
     minWidth: 150,
     maxWidth: 300
   },
   TransportationButton: {
+    backgroundColor: "#3399cc",
+    borderColor: "#426579",
+    flex: 1,
+    height: 140,
+    minWidth: 150,
+    maxWidth: 300
 
   },
   FoodButton: {
+    backgroundColor: "#e49e61",
+    borderColor: "#CC6633",
+    flex: 1,
+    height: 140,
+    minWidth: 150,
+    maxWidth: 300
 
+  },
+  TagIcon: {
+    color: "white",
+    paddingLeft: 10,
+
+  },
+  CompletedTagIcon: {
+    color: "#e6e6e6",
+    paddingLeft: 10,
   }
 })
